8d38cdf3bec0c8bbde14b8017dbde89436db1597
mai095 <maiabdelmoneam095@gmail.com>
2024-05-21 23:18:20 +0300

    modify index

--- src/routes.js
+++ src/routes.js
@@ -28,35 +28,4 @@ const stripe = new Stripe(process.env.STRIPE_KEY);
 const app = express();
 
 // This is your Stripe CLI webhook secret for testing your endpoint locally.
-const endpointSecret = process.env.ENDPOINT_STRIPE_SECRET;
 
-app.post(
-  "/webhook",
-  express.raw({ type: "application/json" }),
-  (request, response) => {
-    const sig = request.headers["stripe-signature"];
-
-    let event;
-
-    try {
-      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-    } catch (err) {
-      response.status(400).send(`Webhook Error: ${err.message}`);
-      return;
-    }
-
-    // Handle the event
-    switch (event.type) {
-      case "checkout.session.completed":
-        const data = event.data.object;
-        console.log(data);
-        break;
-      // ... handle other event types
-      default:
-        console.log(`Unhandled event type ${event.type}`);
-    }
-
-    // Return a 200 response to acknowledge receipt of the event
-    response.send();
-  }
-);

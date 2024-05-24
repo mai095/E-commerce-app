a38109886f034fc42fdde4a53a18fd7986c23a7c
mai095 <maiabdelmoneam095@gmail.com>
2024-05-25 01:08:08 +0300

    test

--- src/modules/order/order.controller.js
+++ src/modules/order/order.controller.js
@@ -10,7 +10,8 @@ const stripe = new Stripe(process.env.STRIPE_KEY);
 //& cashOrder
 export const cashOrder = async (req, res, next) => {
 
-const order = createOrder()
+const order = await createOrder()
+
   //clear cart
   await cartModel.findOneAndUpdate(
     { user: req.userData._id },
--- src/modules/order/order.service.js
+++ src/modules/order/order.service.js
@@ -2,62 +2,60 @@ import { cartModel } from "../../../DB/models/cart.model.js";
 import { orderModel } from "../../../DB/models/order.model.js";
 import productModel from "../../../DB/models/product.model.js";
 
-export function createOrder() {
- return async (req, res, next) => {
-    //get user cart
-    const cart = await cartModel.findOne({ user: req.userData._id });
-    if (!cart) return next(new Error("Cart not found", { cause: 404 }));
+export async function createOrder(req, res, next) {
+  //get user cart
+  const cart = await cartModel.findOne({ user: req.userData._id });
+  if (!cart) return next(new Error("Cart not found", { cause: 404 }));
 
-    //check empty cart
-    if (cart.products.length === 0)
+  //check empty cart
+  if (cart.products.length === 0)
+    return next(
+      new Error("Empty Cart!,Try to add some products", { cause: 400 })
+    );
+
+  //check product
+  for (let i = 0; i < cart.products.length; i++) {
+    const product = await productModel.findById(cart.products[i].productId);
+    if (!product)
       return next(
-        new Error("Empty Cart!,Try to add some products", { cause: 400 })
+        new Error(`This product ${cart.products[i].productId} not found`, {
+          casue: 404,
+        })
       );
 
-    //check product
-    for (let i = 0; i < cart.products.length; i++) {
-      const product = await productModel.findById(cart.products[i].productId);
-      if (!product)
-        return next(
-          new Error(`This product ${cart.products[i].productId} not found`, {
-            casue: 404,
-          })
-        );
+    //check stock
+    if (!product.inStock(cart.products[i].quantity))
+      return next(
+        new Error(
+          `${product._id} Out of stock, Only ${product.quantity} items are available`
+        )
+      );
+  }
 
-      //check stock
-      if (!product.inStock(cart.products[i].quantity))
-        return next(
-          new Error(
-            `${product._id} Out of stock, Only ${product.quantity} items are available`
-          )
-        );
-    }
+  //create order
+  const order = await orderModel.create({
+    ...req.body,
+    user: req.userData._id,
+    products: cart.products.map(
+      ({ productId: { title, price, finalPrice, _id }, quantity }) => ({
+        quantity,
+        product: { title, price, finalPrice, productId: _id },
+      })
+    ),
+  });
+  if (!order) return next(new Error("Order failed", { cause: 400 }));
 
-    //create order
-    const order = await orderModel.create({
-      ...req.body,
-      user: req.userData._id,
-      products: cart.products.map(
-        ({ productId: { title, price, finalPrice, _id }, quantity }) => ({
-          quantity,
-          product: { title, price, finalPrice, productId: _id },
-        })
-      ),
-    });
-    if (!order) return next(new Error("Order failed", { cause: 400 }));
-
-    //update stock by bulkWrite
-    if (order) {
-      const options = cart.products.map((product) => ({
-        updateOne: {
-          filter: { _id: product.productId },
-          update: {
-            $inc: { quantity: -product.quantity, sold: product.quantity },
-          },
+  //update stock by bulkWrite
+  if (order) {
+    const options = cart.products.map((product) => ({
+      updateOne: {
+        filter: { _id: product.productId },
+        update: {
+          $inc: { quantity: -product.quantity, sold: product.quantity },
         },
-      }));
-      await productModel.bulkWrite(options);
-    }
-    return order;
-  };
+      },
+    }));
+    await productModel.bulkWrite(options);
+  }
+  return order;
 }

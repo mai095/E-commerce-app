70d1fc15ac28acddd2f97f9d146591852b83d109
mai095 <maiabdelmoneam095@gmail.com>
2024-05-25 13:01:51 +0300

    test

--- src/modules/order/order.controller.js
+++ src/modules/order/order.controller.js
@@ -90,12 +90,8 @@ export const cancelOrder = async (req, res, next) => {
   if (!order) return next(new Error("Order not found", { cause: 404 }));
 
   //check status
-  if (
-    (order.status !== "placed" && order.payment == "cash") ||
-    (order.status !== "placed" &&
-      order.payment == "visa" &&
-      order.isPaid == true)
-  )
+  if (order.status == "delivered" || order.status == "shipped",
+  order.status == "canceled")
     return next(
       new Error(`Sorry order can't be canceled at status ${order.status}`)
     );

0806ad0b9dcd2e61bfdb18a22db5ecea7b2efbfb
mai095 <maiabdelmoneam095@gmail.com>
2024-05-25 12:47:22 +0300

    edit order schema

--- src/modules/order/order.schema.js
+++ src/modules/order/order.schema.js
@@ -1,4 +1,5 @@
 import joi from "joi";
+import { validationObjectID } from "../../middlewares/validation.js";
 
 // *createOrder
 export const createOrder = joi
@@ -15,6 +16,6 @@ export const createOrder = joi
 // *cancelOrder
 export const cancelOrder = joi
 .object({
-  id: joi.string().custom(validObjectId).required(),
+  id: joi.string().custom(validationObjectID).required(),
 })
 .required();
